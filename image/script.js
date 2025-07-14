44cdbaa624cbedb618ebaa0f8c5cefa82a3acaac
oluwafemi059 <foluwafemi059@gmail.com>
2025-07-14 11:35:34 +0100

    bangees clone file

---
+++ image/script.js
@@ -0,0 +1,20 @@
+const menuIcon = document.getElementById('nav2');
+const navLinks = document.getElementById('nav1');
+
+menuIcon.addEventListener('click', () => {
+    // navLinks.classList.toggle('active');
+});
+
+
+// alert("hello world");
+
+let felicia = document.getElementById("image");
+// console.log(felicia)
+
+ felicia.addEventListener("click", text
+
+ )
+ function text(){
+  felicia.setAttribute("src", "image/logo 2.png")
+ };
+
